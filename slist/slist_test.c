2900bb34ad7f1c14e2a320032c199857ce50b6af
itay-adi <78017074+itay-adi@users.noreply.github.com>
2021-05-09 08:32:31 +0300

    Update slist_test.c

--- slist/slist_test.c
+++ slist/slist_test.c
@@ -1,8 +1,3 @@
-/*
-* File Name - slist_test.c
-* Developer - Itay Adi Yosef
-*/
-
 /***************************User's declarations********************************/
 
 #include <stdio.h> /* printf */
